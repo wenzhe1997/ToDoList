44e367da1281358a585661b6fe43c2b873010545
unknown <wenzhe1997@live.com>
2019-07-08 00:02:44 +0800

    ToDoList

---
+++ PhoneBookWebApi/Controllers/DirectoriesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PhoneBookWebApi.Models;
+using System.Web.Http.Cors;
+
+namespace PhoneBookWebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DirectoriesController : ApiController
+    {
+        private PhoneBookEntities db = new PhoneBookEntities();
+
+        // GET: api/Directories
+        public IQueryable<Directory> GetDirectories()
+        {
+            return db.Directories;
+        }
+
+        // GET: api/Directories/5
+        [ResponseType(typeof(Directory))]
+        public IHttpActionResult GetDirectory(int id)
+        {
+            Directory directory = db.Directories.Find(id);
+            if (directory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(directory);
+        }
+
+        // PUT: api/Directories/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDirectory(int id, Directory directory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != directory.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(directory).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DirectoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Directories
+        [ResponseType(typeof(Directory))]
+        public IHttpActionResult PostDirectory(Directory directory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Directories.Add(directory);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = directory.Id }, directory);
+        }
+
+        // DELETE: api/Directories/5
+        [ResponseType(typeof(Directory))]
+        public IHttpActionResult DeleteDirectory(int id)
+        {
+            Directory directory = db.Directories.Find(id);
+            if (directory == null)
+            {
+                return NotFound();
+            }
+
+            db.Directories.Remove(directory);
+            db.SaveChanges();
+
+            return Ok(directory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DirectoryExists(int id)
+        {
+            return db.Directories.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ PhoneBookWebApi/PhoneBook/crudCtrlAndService.js
@@ -0,0 +1,74 @@
+﻿angular.module('phoneBookApp', []).controller('CrudCtrl', ['$scope', 'crudService', function ($scope, crudService) {
+
+    $scope.createVM = {}
+    $scope.updateVM = {}
+    $scope.showAlert = false;
+
+    $scope.loadTableData = function () {
+        crudService.serviceCall("GET", "", "").then(function (response) {
+            if (response.data.length < 1) {
+                $scope.showAlert = true;
+                $scope.alertName = "Note!"
+                $scope.alertMessage = "These Is No Record To Show."
+            }
+            $scope.phoneBookData = response.data;
+        });
+    }
+
+    $scope.addPhoneNumber = function (isValid) {
+        if (isValid) {
+            $scope.addPhoneNumberForm.$setPristine();
+            crudService.serviceCall("POST", "", $scope.createVM).then(function (response) {
+                $scope.showAlert = true;
+                $scope.alertName = "Success!"
+                $scope.alertMessage = "Task Is Added Successfully."
+                $scope.createVM = {};
+                $scope.loadTableData();
+            });
+        }
+    }
+
+    $scope.openUpdatePhoneNumberModal = function (Id) {
+        crudService.serviceCall("GET", Id, "").then(function (response) {
+            $scope.updateVM = response.data;
+        });
+    }
+
+    $scope.updatePhoneNumber = function (Id) {
+
+        crudService.serviceCall("PUT", Id, $scope.updateVM).then(function (response) {
+            $scope.showAlert = true;
+            $scope.alertName = "Success!"
+            $scope.alertMessage = "Task Is Updated Successfully."
+            $scope.loadTableData();
+        });
+
+    }
+
+    $scope.openDeletePhoneNumberModal = function (Id) {
+        $scope.recordIdToDelete = Id;
+    }
+
+    $scope.deletePhoneNumber = function (Id) {
+        crudService.serviceCall("Delete", Id, "").then(function (response) {
+            $scope.showAlert = true;
+            $scope.alertName = "Success!"
+            $scope.alertMessage = "Task Is Deleted Successfully."
+            $scope.loadTableData();
+        });
+    }
+
+    $scope.loadTableData();
+
+}])
+    .service('crudService', ['$http', function ($http) {
+        return {
+            serviceCall: function (method, parameter, data) {
+                return $http({
+                    method: method,
+                    url: 'https://localhost:44320/api/Directories/' + parameter,
+                    data: data
+                });
+            }
+        };
+    }]);
